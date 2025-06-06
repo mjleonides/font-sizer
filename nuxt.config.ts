fccac4c88de3233cce237d7e07cb9e3181788d5c
mjleonides <mike@leonides.dev>
2025-06-06 18:28:49 -0500

    Readd base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@vite-pwa/nuxt"]
+	modules: ["@vite-pwa/nuxt"],
+	app: {
+		baseURL: "/font-sizer/"
+	}
 });
